8904c49d5e1551ad0661273dd3c607a35aeda49e
afroguy16 <tega_joe@hotmail.com>
2022-11-26 14:27:21 +0000

    use switch case for individual 'as'

--- src/components/Text/index.tsx
+++ src/components/Text/index.tsx
@@ -1,6 +1,6 @@
 import { Fragment, ReactNode } from "react";
 import { TextE } from "./enums";
-import { StyledP } from "./styles";
+import { StyledH1, StyledH2, StyledH3, StyledH4, StyledH5, StyledH6, StyledP } from "./styles";
 
 export interface Prop {
   children: ReactNode,
@@ -11,9 +11,19 @@ const Text = ({children, as}: Prop): JSX.Element => {
   const getContent = () => {
     switch (as) {
       case TextE.P:
-        return <StyledP as={as}>{children}</StyledP>
+        return <StyledP>{children}</StyledP>
       case TextE.H1:
-        return <StyledP as={as}>{children}</StyledP>    
+        return <StyledH1>{children}</StyledH1>
+      case TextE.H2:
+        return <StyledH2>{children}</StyledH2>  
+      case TextE.H3:
+        return <StyledH3>{children}</StyledH3>  
+      case TextE.H4:
+        return <StyledH4>{children}</StyledH4>  
+      case TextE.H5:
+        return <StyledH5>{children}</StyledH5>  
+      case TextE.H6:
+        return <StyledH6>{children}</StyledH6>      
       default:
         return null;
     }
--- src/components/Text/styles.ts
+++ src/components/Text/styles.ts
@@ -10,4 +10,24 @@ export const StyledP = styled.p`
 
 export const StyledH1 = styled.h1`
   ${styles}
+`
+
+export const StyledH2 = styled.h2`
+  ${styles}
+` 
+
+export const StyledH3 = styled.h3`
+  ${styles}
+` 
+
+export const StyledH4 = styled.h4`
+  ${styles}
+` 
+
+export const StyledH5 = styled.h5`
+  ${styles}
+` 
+
+export const StyledH6 = styled.h6`
+  ${styles}
 ` 
